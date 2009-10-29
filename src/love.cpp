6e4a5b16267d95fd424470d0502756a69d83a2a0
rude <rude@localhost>
2009-10-29 22:34:54 +0100

    Fixed love.keyboard.setKeyRepeat, fixed issues with Body:getWhatever, and made the save directory settable.

---
+++ src/love.cpp
@@ -0,0 +1,177 @@
+/**
+* Copyright (c) 2006-2009 LOVE Development Team
+* 
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* 
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 
+* 1. The origin of this software must not be misrepresented; you must not
+*    claim that you wrote the original software. If you use this software
+*    in a product, an acknowledgment in the product documentation would be
+*    appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+*    misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+**/
+
+// LOVE
+#include <common/config.h>
+#include <common/version.h>
+#include <common/runtime.h>
+#include <common/MemoryData.h>
+
+#ifdef LOVE_BUILD_EXE
+
+// SDL
+#include <SDL.h>
+
+// Modules
+#include <audio/wrap_Audio.h>
+#include <event/sdl/wrap_Event.h>
+#include <filesystem/physfs/wrap_Filesystem.h>
+#include <graphics/opengl/wrap_Graphics.h>
+#include <image/wrap_Image.h>
+#include <joystick/sdl/wrap_Joystick.h>
+#include <keyboard/sdl/wrap_Keyboard.h>
+#include <mouse/sdl/wrap_Mouse.h>
+#include <physics/box2d/wrap_Physics.h>
+#include <sound/wrap_Sound.h>
+#include <timer/sdl/wrap_Timer.h>
+
+// Libraries.
+#include "libraries/luasocket/luasocket.h"
+#include "libraries/lanes/lanes.h"
+
+#endif // LOVE_BUILD_EXE
+
+// Resources
+#include "resources/resources.h"
+
+#ifdef LOVE_BUILD_STANDALONE
+
+static const luaL_Reg modules[] = {
+	{ "love.audio", love::audio::luaopen_love_audio },
+	{ "love.event", love::event::sdl::luaopen_love_event },
+	{ "love.filesystem", love::filesystem::physfs::luaopen_love_filesystem },
+	{ "love.graphics", love::graphics::opengl::luaopen_love_graphics },
+	{ "love.image", love::image::luaopen_love_image },
+	{ "love.joystick", love::joystick::sdl::luaopen_love_joystick },
+	{ "love.keyboard", love::keyboard::sdl::luaopen_love_keyboard },
+	{ "love.mouse", love::mouse::sdl::luaopen_love_mouse },
+	{ "love.physics", love::physics::box2d::luaopen_love_physics },
+	{ "love.sound", love::sound::luaopen_love_sound },
+	{ "love.timer", love::timer::sdl::luaopen_love_timer },
+	{ 0, 0 }
+};
+
+#endif // LOVE_BUILD_STANDALONE
+
+extern "C" LOVE_EXPORT int luaopen_love(lua_State * L)
+{
+	love::luax_insistglobal(L, "love");
+
+	// Set version information.
+	lua_pushinteger(L, love::VERSION);
+	lua_setfield(L, -2, "_version");
+
+	lua_pushstring(L, love::VERSION_STR);
+	lua_setfield(L, -2, "_version_string");
+
+	lua_pushstring(L, love::VERSION_CODENAME);
+	lua_setfield(L, -2, "_version_codename");
+
+	lua_newtable(L);
+
+	for(int i = 0; love::VERSION_COMPATIBILITY[i] != 0; ++i)
+	{
+		lua_pushinteger(L, love::VERSION_COMPATIBILITY[i]);
+		lua_rawseti(L, -2, i+1);
+	}
+
+	lua_setfield(L, -2, "_version_compat");
+
+
+	// Resources.
+	for(const love::Resource * r = love::resources; r->name != 0; r++)
+	{
+		love::luax_newtype(L, "Data", love::DATA_T, new love::MemoryData((void*)r->data, r->size));
+		lua_setfield(L, -2, r->name);
+	}
+
+	lua_pop(L, 1); // love
+
+#ifdef LOVE_BUILD_STANDALONE
+
+	// Preload module loaders.
+	for(int i = 0; modules[i].name != 0; i++)
+	{
+		love::luax_preload(L, modules[i].func, modules[i].name);
+	}
+
+	love::luasocket::__open(L);
+	love::lanes::open(L);
+
+#endif // LOVE_BUILD_STANDALONE
+
+	return 0;
+}
+
+#ifdef LOVE_BUILD_EXE
+
+int main(int argc, char ** argv)
+{
+	// Oh, you just want the version? Okay!
+	if(argc > 1 && strcmp(argv[1],"--version") == 0) {
+		printf("This is LOVE %s (%s), the unquestionably awesome 2D game engine.\n", love::VERSION_STR, love::VERSION_CODENAME);
+		return 0;
+	}
+	// Create the virtual machine.
+	lua_State * L = lua_open();
+	luaL_openlibs(L);
+
+	love::luax_preload(L, luaopen_love, "love");
+
+	luaopen_love(L);	
+
+	// Add command line arguments to global arg (like stand-alone Lua).
+	{
+		lua_newtable(L);
+		for(int i = 0;i<argc;i++)
+		{
+			lua_pushstring(L, argv[i]);
+			lua_rawseti(L, -2, i);
+		}
+		lua_setglobal(L, "arg");
+	}
+
+	// Add love.__exe = true.
+	// This indicates that we're running the 
+	// standalone version of love, and not the
+	// DLL version.
+	{
+		lua_getglobal(L, "love");
+		lua_pushboolean(L, 1);
+		lua_setfield(L, -2, "_exe");
+		lua_pop(L, 1);
+	}
+
+	// This is where we should run the built-in Lua code
+	// which gets everything started.
+
+#	include "scripts/boot.lua.h"
+
+	lua_close(L);
+
+#if defined(LOVE_DEBUG) && defined(LOVE_WINDOWS)
+	printf("(press key)\n");
+	getchar();
+#endif
+	printf("Done. This was: %s (%s)\n", love::VERSION_STR, love::VERSION_CODENAME);
+	return 0;
+}
+
+#endif // LOVE_BUILD_EXE
---
+++ src/modules/audio/wrap_Audio.cpp
@@ -0,0 +1,276 @@
+/**
+* Copyright (c) 2006-2009 LOVE Development Team
+* 
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* 
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 
+* 1. The origin of this software must not be misrepresented; you must not
+*    claim that you wrote the original software. If you use this software
+*    in a product, an acknowledgment in the product documentation would be
+*    appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+*    misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+**/
+
+// LOVE
+#include "wrap_Audio.h"
+
+#include "openal/Audio.h"
+#include "null/Audio.h"
+
+namespace love
+{
+namespace audio
+{
+	static Audio * instance = 0;
+
+	int w_getNumSources(lua_State * L)
+	{
+		lua_pushinteger(L, instance->getNumSources());
+		return 1;
+	}
+
+	int w_newSound(lua_State * L)
+	{
+		// Convert to File, if necessary.
+		if(lua_isstring(L, 1))
+			luax_convobj(L, 1, "filesystem", "newFile");
+
+		// Convert to SoundData, if necessary.
+		if(luax_istype(L, 1, FILESYSTEM_FILE_T))
+			luax_convobj(L, 1, "sound", "newSoundData");
+
+		love::sound::SoundData * data = luax_checktype<love::sound::SoundData>(L, 1, "SoundData", SOUND_SOUND_DATA_T);
+		Sound * t = instance->newSound(data);
+		luax_newtype(L, "Sound", AUDIO_SOUND_T, (void*)t);
+		return 1;
+	}
+
+
+	int w_newMusic(lua_State * L)
+	{
+		// Convert to Decoder, if necessary.
+		if(!luax_istype(L, 1, SOUND_DECODER_T))
+			luax_convobj(L, 1, "sound", "newDecoder");
+
+		love::sound::Decoder * decoder = luax_checktype<love::sound::Decoder>(L, 1, "Decoder", SOUND_DECODER_T);
+		Music * t = instance->newMusic(decoder);
+		luax_newtype(L, "Music", AUDIO_MUSIC_T, (void*)t);
+		return 1;
+	}
+
+	int w_newSource(lua_State * L)
+	{
+		Audible * a = luax_checktype<Audible>(L, 1, "Audible", AUDIO_AUDIBLE_T);
+		Source * t = instance->newSource(a);
+		luax_newtype(L, "Source", AUDIO_SOURCE_T, (void*)t);
+		return 1;
+	}
+
+	int w_play(lua_State * L)
+	{
+		int argn = lua_gettop(L);
+
+		if(luax_istype(L, 1, AUDIO_SOUND_T))
+		{
+			Sound * s = luax_checksound(L, 1);
+			instance->play(s);
+			return 0;
+		} 
+		else if(luax_istype(L, 1, AUDIO_MUSIC_T))
+		{
+			Music * m = luax_checkmusic(L, 1);
+			instance->play(m);
+			return 0;
+		}
+		else if(luax_istype(L, 1, AUDIO_SOURCE_T))
+		{
+			Source * s = luax_checksource(L, 1);
+			instance->play(s);
+			return 0;
+		}
+
+		return luaL_error(L, "No matching overload");
+	}
+
+	int w_stop(lua_State * L)
+	{
+		instance->stop();
+		return 0;
+	}
+
+	int w_pause(lua_State * L)
+	{
+		instance->pause();
+		return 0;
+	}
+
+	int w_resume(lua_State * L)
+	{
+		instance->resume();
+		return 0;
+	}
+
+	int w_rewind(lua_State * L)
+	{
+		instance->rewind();
+		return 0;
+	}
+
+	int w_setVolume(lua_State * L)
+	{
+		float v = (float)luaL_checknumber(L, 1);
+		instance->setVolume(v);
+		return 0;
+	}
+
+	int w_getVolume(lua_State * L)
+	{
+		lua_pushnumber(L, instance->getVolume());
+		return 1;
+	}
+
+	int w_setPosition(lua_State * L)
+	{
+		float v[3];
+		v[0] = (float)luaL_checknumber(L, 1);
+		v[1] = (float)luaL_checknumber(L, 2);
+		v[2] = (float)luaL_checknumber(L, 3);
+		instance->setPosition(v);
+		return 0;
+	}
+
+	int w_getPosition(lua_State * L)
+	{
+		float v[3];
+		instance->getPosition(v);
+		lua_pushnumber(L, v[0]);
+		lua_pushnumber(L, v[1]);
+		lua_pushnumber(L, v[2]);
+		return 3;
+	}
+
+	int w_setOrientation(lua_State * L)
+	{
+		float v[6];
+		v[0] = (float)luaL_checknumber(L, 1);
+		v[1] = (float)luaL_checknumber(L, 2);
+		v[2] = (float)luaL_checknumber(L, 3);
+		v[3] = (float)luaL_checknumber(L, 4);
+		v[4] = (float)luaL_checknumber(L, 5);
+		v[5] = (float)luaL_checknumber(L, 6);
+		instance->setOrientation(v);
+		return 0;
+	}
+
+	int w_getOrientation(lua_State * L)
+	{
+		float v[6];
+		instance->getOrientation(v);
+		lua_pushnumber(L, v[0]);
+		lua_pushnumber(L, v[1]);
+		lua_pushnumber(L, v[2]);
+		lua_pushnumber(L, v[3]);
+		lua_pushnumber(L, v[4]);
+		lua_pushnumber(L, v[5]);
+		return 6;
+	}
+
+	int w_setVelocity(lua_State * L)
+	{
+		float v[3];
+		v[0] = (float)luaL_checknumber(L, 1);
+		v[1] = (float)luaL_checknumber(L, 2);
+		v[2] = (float)luaL_checknumber(L, 3);
+		instance->setVelocity(v);
+		return 0;
+	}
+
+	int w_getVelocity(lua_State * L)
+	{
+		float v[3];
+		instance->getVelocity(v);
+		lua_pushnumber(L, v[0]);
+		lua_pushnumber(L, v[1]);
+		lua_pushnumber(L, v[2]);
+		return 3;
+	}
+
+	// List of functions to wrap.
+	static const luaL_Reg functions[] = {
+		{ "getNumSources", w_getNumSources },
+		{ "newSound", w_newSound },
+		{ "newMusic", w_newMusic },
+		{ "newSource", w_newSource },
+		{ "play", w_play },
+		{ "stop", w_stop },
+		{ "pause", w_pause },
+		{ "resume", w_resume },
+		{ "rewind", w_rewind },
+		{ "setVolume", w_setVolume },
+		{ "getVolume", w_getVolume },
+		{ "setPosition", w_setPosition },
+		{ "getPosition", w_getPosition },
+		{ "setOrientation", w_setOrientation },
+		{ "getOrientation", w_getOrientation },
+		{ "setVelocity", w_setVelocity },
+		{ "getVelocity", w_getVelocity },
+		{ 0, 0 }
+	};
+
+	static const lua_CFunction types[] = {
+		luaopen_source,
+		luaopen_music,
+		luaopen_sound,
+		0
+	};	
+
+	int luaopen_love_audio(lua_State * L)
+	{
+		if(instance == 0)
+		{
+			// Try OpenAL first.
+			try 
+			{
+				instance = new love::audio::openal::Audio();
+			}
+			catch(love::Exception & e)
+			{
+				std::cout << e.what() << std::endl;
+			}
+		}
+
+		if(instance == 0)
+		{
+			// Fall back to nullaudio.
+			try
+			{
+				instance = new love::audio::null::Audio();
+			}
+			catch(love::Exception & e)
+			{
+				std::cout << e.what() << std::endl;
+			}
+		}
+
+		if(instance == 0)
+			return luaL_error(L, "Could not open any audio module.");
+
+		WrappedModule w;
+		w.module = instance;
+		w.name = "audio";
+		w.flags = MODULE_T;
+		w.functions = functions;
+		w.types = types;
+
+		return luax_register_module(L, w);
+	}
+
+} // audio
+} // love
